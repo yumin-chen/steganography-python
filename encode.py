4ec3fd4359df3d51e1a919fe99e04d442e88373a
Chen Yumin <hello@chenyumin.com>
2016-12-06 22:50:34 +0000

    Organize code and add comments

--- encode.py
+++ encode.py
@@ -1,20 +1,24 @@
 import cv2 # OpenCV 2
 
-# Create a hidden message
-def hidden_message():
-    msg = "This is the hidden message......" * 10000
+def to_bit_generator(msg):
+    """Converts a message into a generator which returns 1 bit of the message
+    each time."""
     for c in (msg):
         o = ord(c)
         for i in range(8):
             yield (o & (1 << i)) >> i
 
-hiddenMessage = hidden_message()
+hiddenMessage = to_bit_generator("This is the hidden message......" * 10000)
 
-# Read the original image
-img = cv2.imread('img.jpg', cv2.IMREAD_GRAYSCALE)
-for h in range(len(img)):
-    for w in range(len(img[0])):
-        # Write the hidden message into the least significant bit
-        img[h][w] = (img[h][w] & ~1) | next(hiddenMessage)
-# Write out the image with hidden message
-cv2.imwrite("img_grayscale.bmp", img)
+def main():
+    # Read the original image
+    img = cv2.imread('img.jpg', cv2.IMREAD_GRAYSCALE)
+    for h in range(len(img)):
+        for w in range(len(img[0])):
+            # Write the hidden message into the least significant bit
+            img[h][w] = (img[h][w] & ~1) | next(hiddenMessage)
+    # Write out the image with hidden message
+    cv2.imwrite("img_grayscale.bmp", img)
+
+if __name__ == "__main__":
+	main()
